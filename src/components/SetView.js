f4fc6769d32b52f926bd57a9c095acd8ac962cbd
unknown <dlwp142@gmail.com>
2020-11-28 02:05:30 +0900

    set notification emoji

--- src/components/SetView.js
+++ src/components/SetView.js
@@ -11,34 +11,34 @@ const SetView = () => {
     const dispatch = useDispatch();
     const [work_time, set_work_time] = useState(25);
     const [rest_time, set_rest_time] = useState(5);
-    const [work_input_borderColor,set_work_input_borderColor] = useState("white");
-    const [rest_input_borderColor,set_rest_input_borderColor] = useState("white");
+    const [work_input_borderColor, set_work_input_borderColor] = useState("white");
+    const [rest_input_borderColor, set_rest_input_borderColor] = useState("white");
 
     function press_button() {
         set_work_input_borderColor("white");
         set_rest_input_borderColor("white");
         if (work_time == undefined || isNaN(Number(work_time)) || work_time == 0) {
             set_work_input_borderColor("red");
-            return;            
+            return;
         }
         if (rest_time == undefined || isNaN(Number(rest_time)) || rest_time == 0) {
             set_rest_input_borderColor("red");
-            return;        
+            return;
         }
 
-        Notification.register({message: "\u2615", time: work_time*60 });
+        Notification.register({ message: "\u231B \u2705", time: work_time * 60 });
         dispatch(ActionCreators.setStartOptions({ work_minute: work_time * 60, rest_minute: rest_time * 60 }));
     }
 
     return <View style={styles.setContainer}>
         <View style={styles.viewLine}>
             <HourglassIcon />
-            <TextInput style={{...styles.textInput,borderColor:work_input_borderColor}} onChangeText={text => { set_work_time(text) }} keyboardType="number-pad" placeholder={"25"} />
+            <TextInput style={{ ...styles.textInput, borderColor: work_input_borderColor }} onChangeText={text => { set_work_time(text) }} keyboardType="number-pad" placeholder={"25"} />
 
         </View>
         <View style={styles.viewLine}>
             <RestIcon />
-            <TextInput style={{...styles.textInput,borderColor:rest_input_borderColor}} onChangeText={text => { set_rest_time(text) }} keyboardType="number-pad" placeholder={"5"} />
+            <TextInput style={{ ...styles.textInput, borderColor: rest_input_borderColor }} onChangeText={text => { set_rest_time(text) }} keyboardType="number-pad" placeholder={"5"} />
         </View>
         <TouchableOpacity style={styles.TouchableOpacity} onPress={press_button}><StartIcon /></TouchableOpacity>
     </View>
--- src/components/TimerView.js
+++ src/components/TimerView.js
@@ -4,8 +4,8 @@ import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { useSelector, useDispatch } from 'react-redux';
 import ActionCreators from '../actions';
 import Notification from '../Notification';
-import {styles,colors} from '../styles';
-import {StopIcon} from './Icons';
+import { styles, colors } from '../styles';
+import { StopIcon } from './Icons';
 
 const TimerView = () => {
     const dispatch = useDispatch();
@@ -15,14 +15,14 @@ const TimerView = () => {
     }
     return <View style={styles.container}>
         <TimerAnimation />
-        <TouchableOpacity style={styles.TouchableOpacity} onPress={() => { press_button(); }}><StopIcon/></TouchableOpacity>
+        <TouchableOpacity style={styles.TouchableOpacity} onPress={() => { press_button(); }}><StopIcon /></TouchableOpacity>
     </View>
 }
 
 const TimerAnimation = () => {
     const dispatch = useDispatch();
-    const { work_minute, rest_minute, work_flag, rest_flag} = useSelector(state => state.options);
-    
+    const { work_minute, rest_minute, work_flag, rest_flag } = useSelector(state => state.options);
+
     var start = Date.now();
     const TimeView = () => {
         const [ti, setTi] = useState((work_flag ? work_minute : rest_minute));
@@ -36,35 +36,35 @@ const TimerAnimation = () => {
                 clearInterval(tti);
             }
         }, [ti]);
-        return <Text style={{color:"white", fontSize: 40 }}>{parseInt(ti / 60)}:{ti % 60}</Text>
+        return <Text style={{ color: "white", fontSize: 40 }}>{parseInt(ti / 60)}:{(ti % 60 < 10 ? "0" + String(ti % 60) : ti % 60)}</Text>
     }
 
     const _completeHandle = () => {
         if (work_flag) {
-            Notification.register({message:"🛑",time:rest_minute});
+            Notification.register({ message: "\u2615 \u2705", time: rest_minute });
             dispatch(ActionCreators.endWork());
         }
-        else if(rest_flag) {
+        else if (rest_flag) {
             dispatch(ActionCreators.endRest());
         }
     }
 
     return <AnimatedCircularProgress
-            size={300}
-            width={40}
-            fill={100}
-            rotation={work_flag ? 210 : -330}
-            tintColor={(work_flag ? colors.green : colors.red)}
-            tintColorSecondary={(work_flag ? colors.red : colors.green)}
-            duration={((work_flag ? work_minute : rest_minute) * 1000)}
-            backgroundColor="#3d5875"
-            lineCap="round"
-            arcSweepAngle={work_flag ? 300 : 300}
-            easing = {Easing.linear}
-            onAnimationComplete={_completeHandle}
-        >
-            {(fill) => (<TimeView />)}
-        </AnimatedCircularProgress>
+        size={300}
+        width={40}
+        fill={100}
+        rotation={work_flag ? 210 : -330}
+        tintColor={(work_flag ? colors.green : colors.red)}
+        tintColorSecondary={(work_flag ? colors.red : colors.green)}
+        duration={((work_flag ? work_minute : rest_minute) * 1000)}
+        backgroundColor="#3d5875"
+        lineCap="round"
+        arcSweepAngle={work_flag ? 300 : 300}
+        easing={Easing.linear}
+        onAnimationComplete={_completeHandle}
+    >
+        {(fill) => (<TimeView />)}
+    </AnimatedCircularProgress>
 }
 
 export default TimerView;
